d1b4bd3168ac7c03c8bf12e8a5962bc954c70866
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-01 16:29:29 +0100

    Created Navigation component and pages

---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+const { default: Link } = require("next/link");
+
+const Navigation = () => {
+  return (
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/pokemon">Pokemon</Link>
+      </li>
+      <li>
+        <Link href="/pokemonSSR">Mis Pokemon SSR</Link>
+      </li>
+      <li>
+        <Link href="/pokemonSSG">Mis Pokemon SSG</Link>
+      </li>
+      <li>
+        <Link href="/pokemonISR">Mis Pokemon ISR</Link>
+      </li>
+    </ul>
+  );
+};
