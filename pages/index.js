816297abf078b6045d05f0f73eba18d016883ea9
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-01 17:42:04 +0100

    Added styles to title

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className="title">{`Ed's Next Pokemon`}</h1>
+      <h2>{`Go to one of the pages to view Pokemon!`}</h2>
     </div>
   );
 }
