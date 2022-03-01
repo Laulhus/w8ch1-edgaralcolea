2f20e0404d55cdc5aaa0cf1a8104e1af5dfc36f4
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-01 17:19:34 +0100

    Added useEffect hook to get Pokemon in CSR Page

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -2,23 +2,25 @@ const { default: Link } = require("next/link");
 
 const Navigation = () => {
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/pokemon">Pokemon</Link>
-      </li>
-      <li>
-        <Link href="/pokemonSSR">Mis Pokemon SSR</Link>
-      </li>
-      <li>
-        <Link href="/pokemonSSG">Mis Pokemon SSG</Link>
-      </li>
-      <li>
-        <Link href="/pokemonISR">Mis Pokemon ISR</Link>
-      </li>
-    </ul>
+    <div className="nav-container">
+      <ul className="navigation">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/pokemon">Pokemon</Link>
+        </li>
+        <li>
+          <Link href="/pokemonSSR">Mis Pokemon SSR</Link>
+        </li>
+        <li>
+          <Link href="/pokemonSSG">Mis Pokemon SSG</Link>
+        </li>
+        <li>
+          <Link href="/pokemonISR">Mis Pokemon ISR</Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
